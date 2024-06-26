ccb24175d1ab2c78ccce489231ca1ab2476f2649
Admin <smpratik888@gmail.com>
2024-06-26 18:06:45 +0530

    updaing object lis

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,7 +7,7 @@ const initialGameBoard = [
 ];
 
 
-export default function GameBoard() {
+export default function GameBoard( {onSelectSquare}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
     
     function handleSelectSquare(rowIndex, colIndex){
@@ -16,6 +16,7 @@ export default function GameBoard() {
             updatedBoard[rowIndex][colIndex] = 'X';
             return updatedBoard;
         });
+        onSelectSquare();
     }
 
     return (
