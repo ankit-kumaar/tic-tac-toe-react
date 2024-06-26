6176c021e7e439e4b53a944a15dbceb5a92d7c7c
Admin <smpratik888@gmail.com>
2024-06-26 11:33:53 +0530

    updaing object list

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -20,7 +20,7 @@ export default function GameBoard() {
 
     return (
     <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
+        {gameBoard.map((row, rowIndex) => (
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
